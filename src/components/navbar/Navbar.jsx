684c20dad1bca1764754e8ac707451028a50ce2b
Sam W <sam@sammymayhem.com>
2023-04-14 20:08:07 -0700

    Added images to assets, started on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import './navbar.css';
 
 const Navbar = () => {
   return (
