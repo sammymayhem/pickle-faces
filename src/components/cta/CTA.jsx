647fec86a524795715c47667d0ef568fc731054b
Sam W <sam@sammymayhem.com>
2023-04-18 12:22:46 -0700

    Added the CTA section with quick verbiage. Button is active, but does not lead anywhere yet.

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './cta.css';
 
-const  Cta = () => {
+const CTA = () => {
   return (
-    <div>
-      CTA
+    <div className='pickle__cta'>
+      <div className='pickle__cta-content'>
+        <p>Would you like to learn more about pickles?</p>
+        <h3>Sign up for our pickle newsletter & start exploring the endless possiblities.</h3>
+      </div>
+      <div className='pickle__cta-btn'>
+        <button type='button'>Get Started</button>
+      </div>
     </div>
   )
 }
 
-export default Cta
+export default CTA
