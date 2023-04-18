3789e63036fc62ec9e93dc81462fc3e5c94fb3db
Sam W <sam@sammymayhem.com>
2023-04-18 11:59:38 -0700

    Structured the features section and styled. Updated info with pickle ideas.

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './features.css';
+import { Feature } from '../../components';
+
+const featuresData = [
+  {
+    title: 'Improving your sandwich or burger',
+    text: 'If you enjoy a vinegary, dill floavor with a hint of garlic, throw some of those pickles on your sandwich or burger! You will thank me later!'
+  },
+  {
+    title: 'Add something extra to your drink',
+    text: 'Whether its a bloody mary, red snapper or following a shot with picke juice, you can improve your drinks with a little bit of pickle.'
+  },
+  {
+    title: 'Deep fry those pickles',
+    text: 'You know what makes everything better? Deep frying! Not that you needed pickles to get even better, but after a deep fry...be ready for your mind to be blown!'
+  },
+  {
+    title: 'Dill Pickle Dip',
+    text: 'Just when you thought it could not get any better. BOOM! Make a pickle dip and watch as your friends melt into happiness as your dip keeps them hooked to the bottom of the bowl.'
+  },
+]
 
 const Features = () => {
   return (
-    <div>
-      Features
+    <div className='gpt3__features section__padding' id='features'>
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'>The Future is Now and You Just Need To a Pickle to Start. Step into Briny Future Today & Make it Pickled.</h1>
+      </div>
+      <div className='gpt3__features-container'>
+        {featuresData.map((item, index) => (
+          <Feature title={item.title} text={item.text} key={item.title + index} />
+        ))}
+      </div>
     </div>
   )
 }
--- src/containers/whatPickle/WhatPickle.jsx
+++ src/containers/whatPickle/WhatPickle.jsx
@@ -2,26 +2,6 @@ import React from 'react';
 import './whatPickle.css';
 import { Feature } from '../../components';
 
-// const factsData = [
-//   {
-//     title: 'What is a pickle?',
-//     text: 'What most Americans simply call pickles are pickled cucumbers. Most people recognize pickles as the deliciously sweet and sour green food we eat with our sandwiches, on top of burgers or as a tasty snack. However, many don’t realize that pickles are made from cucumbers and that other vegetables, fruits and other foods can also be pickled.'
-//   },
-//   {
-//     title: 'Taming Tummy Troubles',
-//     text: 'Thanks to the fact that some pickles are fermented, they can be a source of probiotics. Eating foods that contain probiotics like sour pickles can help restore balance and keep tummy woes at bay.'
-//   },
-//   {
-//     title: 'Strengthens Bones',
-//     text: 'Pickles are an excellent source of vitamin K, a nutrient needed to make a protein called osteocalcin, a building block of bones.'
-//   },
-//   {
-//     title: 'Balances Blood Sugar',
-//     text: 'Pickles may be a good snack for people with diabetes since many varieties (such as sour, kosher and dill as opposed to sweet or bread and butter styles) contain little to no carbohydrates. That means pickles won’t make blood glucose rise the way other quick eats (such as cookies or potato chips) do.'
-//   }
-
-// ]
-
 const WhatPickle = () => {
   return (
     <div className='pickle__whatPickle section__margin' id='whatpickle'>
@@ -29,7 +9,7 @@ const WhatPickle = () => {
         <Feature title='What is a pickle?' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.' />
       </div>
       <div className='pickle__whatPickle-heading'>
-        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <h1 className='gradient__text'>Explore the benefits of pickles!</h1>
       </div>
       <div className='pickle__whatPickle-container'>
         <Feature title='Taming Tummy Troubles' text='Thanks to the fact that some pickles are fermented, they can be a source of probiotics. Eating foods that contain probiotics like sour pickles can help restore balance and keep tummy woes at bay.' />
